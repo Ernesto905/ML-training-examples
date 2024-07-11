414fdde405b08db3188c0810ae0e0815df8310ef
Ernesto905 <ernesto90643@gmail.com>
2024-07-10 18:29:19 -0700

    Reduced to 1 epoch

--- distilbert_fine_tuning/train.py
+++ distilbert_fine_tuning/train.py
@@ -3,13 +3,13 @@ from composer import Trainer
 from composer.models import HuggingFaceModel
 from composer.callbacks import MLFlowLogger
 
-
+import mlflow
 import torch
 
 # Hugging face imports
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
-import os 
+import os
 
 # Personal imports
 from inference import inference
@@ -31,17 +31,18 @@ else:
     device = "cpu"
 print(f"Using device: {device}")
 
-# Define our hf model and use composer to wrap it
-model_name = "distilbert-base-uncased"
-hf_model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=2)
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = HuggingFaceModel(model=hf_model, tokenizer=tokenizer)
-
-
-mlflow.log_param("device", device)
-mlflow.log_param("model_name", model_name)
 
 with mlflow.start_run():
+    # Define our hf model and use composer to wrap it
+    model_name = "distilbert-base-uncased"
+    hf_model = AutoModelForSequenceClassification.from_pretrained(
+        model_name, num_labels=2
+    )
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = HuggingFaceModel(model=hf_model, tokenizer=tokenizer)
+
+    mlflow.log_param("device", device)
+    mlflow.log_param("model_name", model_name)
     # Load and preprocess data
     train_loader, val_loader, test_loader = load_and_preprocess_data(tokenizer)
 
@@ -54,8 +55,11 @@ with mlflow.start_run():
     print(f"Sarcasm probability: {sarcasm_probability:.2f}%")
 
     # Set a tag that we can use to remind ourselves what this run was for
-    mlflow.set_tag("Training Info", """Fine tuning job on distilbert for news
-        headline classification""")
+    mlflow.set_tag(
+        "Training Info",
+        """Fine tuning job on distilbert for news
+        headline classification""",
+    )
     mlflow.log_metric("initial_accuracy", sarcasm_probability)
     mlflow_logger = MLFlowLogger()
 
@@ -64,8 +68,8 @@ with mlflow.start_run():
         model=model,
         train_dataloader=train_loader,
         eval_dataloader=val_loader,
-        max_duration="2ep",
-        callbacks=[mlflow_logger]
+        max_duration="1ep",
+        callbacks=[mlflow_logger],
     )
 
     # Train the model
