fe7dc7aebb206ef1b0214920943033d59b608764
Ernesto905 <ernesto90643@gmail.com>
2024-07-10 18:22:31 -0700

    Added mlflow experiment tracking to distil bert job

--- distilbert_fine_tuning/train.py
+++ distilbert_fine_tuning/train.py
@@ -1,15 +1,22 @@
 # MosaicML imports
 from composer import Trainer
 from composer.models import HuggingFaceModel
+from composer.callbacks import MLFlowLogger
+
 
 import torch
 
 # Hugging face imports
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
+import os 
+
 # Personal imports
 from inference import inference
 from preprocess import load_and_preprocess_data
+
+mlflow.set_tracking_uri(os.environ.get("TRACKING_ARN"))
+mlflow.set_experiment("Presentation")
 
 message = """Breaking news! Leading world class scientists at MIT have
 gathered to discuss whether water is, in fact, wet."""
@@ -30,32 +37,46 @@ hf_model = AutoModelForSequenceClassification.from_pretrained(model_name, num_la
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = HuggingFaceModel(model=hf_model, tokenizer=tokenizer)
 
-# Load and preprocess data
-train_loader, val_loader, test_loader = load_and_preprocess_data(tokenizer)
 
-# Before we train our model, let's test how good it is at the task
-sarcasm_probability = inference(
-    model=model,
-    tokenizer=tokenizer,
-    text=message,
-)
-print(f"Sarcasm probability: {sarcasm_probability:.2f}%")
+mlflow.log_param("device", device)
+mlflow.log_param("model_name", model_name)
 
-# Set up the trainer
-trainer = Trainer(
-    model=model,
-    train_dataloader=train_loader,
-    eval_dataloader=val_loader,
-    max_duration="2ep",
-)
+with mlflow.start_run():
+    # Load and preprocess data
+    train_loader, val_loader, test_loader = load_and_preprocess_data(tokenizer)
 
-# Train the model
-trainer.fit()
+    # Before we train our model, let's test how good it is at the task
+    sarcasm_probability = inference(
+        model=model,
+        tokenizer=tokenizer,
+        text=message,
+    )
+    print(f"Sarcasm probability: {sarcasm_probability:.2f}%")
 
-# After training, test again
-sarcasm_probability = inference(
-    model=model,
-    tokenizer=tokenizer,
-    text=message,
-)
-print(f"Sarcasm probability after training: {sarcasm_probability:.2f}%")
+    # Set a tag that we can use to remind ourselves what this run was for
+    mlflow.set_tag("Training Info", """Fine tuning job on distilbert for news
+        headline classification""")
+    mlflow.log_metric("initial_accuracy", sarcasm_probability)
+    mlflow_logger = MLFlowLogger()
+
+    # Set up the trainer
+    trainer = Trainer(
+        model=model,
+        train_dataloader=train_loader,
+        eval_dataloader=val_loader,
+        max_duration="2ep",
+        callbacks=[mlflow_logger]
+    )
+
+    # Train the model
+    trainer.fit()
+
+    # After training, test again
+    sarcasm_probability = inference(
+        model=model,
+        tokenizer=tokenizer,
+        text=message,
+    )
+    print(f"Sarcasm probability after training: {sarcasm_probability:.2f}%")
+
+    mlflow.log_metric("final_accuracy", sarcasm_probability)
