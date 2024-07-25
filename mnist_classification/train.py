fdb4d254e354eb8ec58e2887bbd23385ffd0e897
Ernesto905 <ernesto90643@gmail.com>
2024-07-25 08:24:22 -0700

    Revert

--- mnist_classification/train.py
+++ mnist_classification/train.py
@@ -12,22 +12,10 @@ import os
 import mlflow
 from sagemaker_training import environment
 
-import torch.distributed as dist
-from torch.nn.parallel import DistributedDataParallel as DDP
 
 
-
-training_env = environment.Environment()
-smdataparallel_enabled = training_env.additional_framework_parameters.get('sagemaker_distributed_dataparallel_enabled', False)
-
-if smdataparallel_enabled:
-    try:
-        import smdistributed.dataparallel.torch.torch_smddp
-        from smdistributed.dataparallel.torch.torch_smddp import DistributedDataParallel as DDP
-        backend = 'smddp'
-        print('Using smddp as backend')
-    except ImportError: 
-        print('smdistributed module not available, falling back to NCCL collectives.')
+import smdistributed.dataparallel.torch.torch_smddp as dist
+from smdistributed.dataparallel.torch.torch_smddp import DistributedDataParallel as DDP
 
 dist.init_process_group()
 
